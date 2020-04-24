760533213713a34e5663889126978929eecc19b3
gjj2828 <gjj2828@gmail.com>
2020-04-24 15:45:26 +0800

    MonoBehaviour 状态变化时相关消息调用顺序的研究

---
+++ Assets/BehaviourEvent.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class BehaviourEvent : MonoBehaviour
+{
+    private string mName;
+    private bool mIsLogUpdate = false;
+
+    private void Awake() {
+        mName = gameObject.name;
+        Log("Awake");
+    }
+
+    private void OnEnable() {
+        Log("OnEnable");
+    }
+
+    private void OnDisable() {
+        Log("OnDisable");
+    }
+
+    private void OnDestroy() {
+        Log("OnDestroy");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!mIsLogUpdate) {
+            Log("First Update");
+            mIsLogUpdate = true;
+        }
+    }
+
+    void Log(string evt) {
+        Debug.LogFormat("{0} {1} at frame {2}", mName, evt, Time.frameCount);
+    }
+}
---
+++ Assets/BehaviourEventTester.cs
@@ -0,0 +1,59 @@
+﻿//using UnityEditor;
+using UnityEngine;
+
+public class BehaviourEventTester : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private GameObject mA;
+    private GameObject mB;
+    //private GameObject mC;
+    //private GameObject mD;
+    private GameObject mE;
+    private GameObject mF;
+
+    private bool mIsFirstUpdate = true;
+
+    private void Awake() {
+        mF = new GameObject("F", typeof(BehaviourEvent));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mA = new GameObject("A", typeof(BehaviourEvent));
+        mB = new GameObject("B", typeof(BehaviourEvent));
+        //GameObject go = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/BehaviourEvent.prefab");
+        //mC = Instantiate(prefab);
+        //mD = new GameObject("D");
+        //mD.AddComponent<BehaviourEvent>();
+        Invoke("DisableAll", 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mIsFirstUpdate) {
+            mE = new GameObject("E", typeof(BehaviourEvent));
+            mIsFirstUpdate = false;
+        }
+    }
+
+    void DisableAll() {
+        mA.SetActive(false);
+        mB.SetActive(false);
+        Invoke("EnableAll", 1.0f);
+    }
+
+    void EnableAll() {
+        mA.SetActive(true);
+        mB.SetActive(true);
+        Invoke("DestroyAll", 1.0f);
+    }
+
+    void DestroyAll() {
+        Object.Destroy(mA);
+        Object.Destroy(mB);
+        //Object.Destroy(mD.GetComponent<BehaviourEvent>());
+    }
+}
